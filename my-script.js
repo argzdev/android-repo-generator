e4a71352e65ae50d336344b822f277d299f066bb
argzdev <alvinrustan@google.com>
2023-02-20 22:00:23 +0800

    test

--- my-script.js
+++ my-script.js
@@ -1,11 +1,19 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+const owner = "firebase"
+const repo = "firebase-android-sdk"
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN
+})
 
 async function run() {
-  const GITHUB_TOKEN = process.env.GITHUB_TOKEN
-  const octokit = github.getOctokit(GITHUB_TOKEN)
+  // const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+  // const octokit = github.getOctokit(GITHUB_TOKEN)
 
-  const { data } = await octokit.repos.listForAuthenticatedUser();
+await octokit.request('GET /repos/{owner}/{repo}/issues', {
+  owner: owner,
+  repo: repo
+})
+
   console.log(data);
 }
 
