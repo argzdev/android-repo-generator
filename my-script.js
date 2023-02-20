1866555e50d1471f361915836795e3791380c656
argzdev <alvinrustan@google.com>
2023-02-20 22:29:01 +0800

    test

--- my-script.js
+++ my-script.js
@@ -8,25 +8,40 @@ const octokit = new Octokit({
 })
 
 async function run() {
-  const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
-    owner: owner,
-    repo: repo,
-    state: "open"
-  })
+  // const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+  //   owner: owner,
+  //   repo: repo,
+  //   state: "open"
+  // })
+  // console.log(data.length);
 
-  console.log(data.length);
+  const today = new Date().toISOString().split('T')[0];
+  const query = `is:issue is:open created:${today}`;
+
+  octokit.search.issuesAndPullRequests({ q: query })
+    .then((response) => {
+      console.log(`Found ${response.data.total_count} issues opened today:`);
+      response.data.items.forEach((issue) => {
+        console.log(`- ${issue.html_url}`);
+      });
+    })
+    .catch((error) => {
+      console.error(`Error retrieving issues: ${error}`);
+    });
+    
 
 
-  octokit.repos.createForAuthenticatedUser({
-    name: "test_repo",
-    description: "this is a test",
-  })
-  .then((response) => {
-    console.log(`Successfully created new repository: ${response.data.html_url}`);
-  })
-  .catch((error) => {
-    console.error(`Error creating new repository: ${error}`);
-  });
+
+  // octokit.repos.createForAuthenticatedUser({
+  //   name: "test_repo",
+  //   description: "this is a test",
+  // })
+  // .then((response) => {
+  //   console.log(`Successfully created new repository: ${response.data.html_url}`);
+  // })
+  // .catch((error) => {
+  //   console.error(`Error creating new repository: ${error}`);
+  // });
 }
 
 run();
