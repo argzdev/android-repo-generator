7379a481bcbb9c290ea31f441bd778c271b8a6d7
argzdev <alvinrustan@google.com>
2023-02-20 21:51:07 +0800

    test

--- my-script.js
+++ my-script.js
@@ -2,7 +2,7 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 async function run() {
-  const GITHUB_TOKEN = process.env.MY_TOKEN
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN
   const octokit = github.getOctokit(GITHUB_TOKEN)
 
   const { data } = await octokit.repos.listForAuthenticatedUser();
