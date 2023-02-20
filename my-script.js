6cff10b791b73634ed4e120baccfd1b2a2fa4933
argzdev <alvinrustan@google.com>
2023-02-20 21:18:02 +0800

    changed retrieval of octokit

--- my-script.js
+++ my-script.js
@@ -3,7 +3,7 @@ const github = require('@actions/github')
 
 async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
-  const octokit = github.getOctoKit(GITHUB_TOKEN)
+  const octokit = github.getOctokit(GITHUB_TOKEN)
 
   const { data } = await octokit.repos.listForAuthenticatedUser();
   console.log(data);
