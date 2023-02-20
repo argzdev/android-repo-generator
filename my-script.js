f34c4d2821e5d47181596af8cc36aa7f9f79dec3
argzdev <alvinrustan@google.com>
2023-02-20 21:37:06 +0800

    test

--- my-script.js
+++ my-script.js
@@ -1,5 +1,8 @@
 async function run() {
-  console.log(process.env.MY_NAME)
+  console.log("Starting...")
+  console.log("MY NAME: " + process.env.MY_NAME)
+  console.log("MY EMAIL: " + process.env.MY_EMAIL)
+  console.log("MY USERNAME: " + process.env.MY_USERNAME)
 }
 
 run();
