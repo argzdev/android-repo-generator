46167ad6de3cc5187ee84f8491177fb46bab16bd
argzdev <alvinrustan@google.com>
2023-02-20 20:16:50 +0800

    added functionalities

--- my-script.js
+++ my-script.js
@@ -1,10 +1,10 @@
-const { Octokit } = require("@octokit/rest");
-
-const octokit = new Octokit({
-  auth: process.env.MY_TOKEN,
-});
+const core = require('@actions/core')
+const github = require('@actions/github')
 
 async function run() {
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+  const octokit = github.getOctoKit(GITHUB_TOKEN)
+
   const { data } = await octokit.repos.listForAuthenticatedUser();
   console.log(data);
 }
