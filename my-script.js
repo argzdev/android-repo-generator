0a9468456de1a2004a8792e81d29257eb7d577e1
argzdev <alvinrustan@google.com>
2023-02-22 19:08:57 +0800

    testing with files

--- my-script.js
+++ my-script.js
@@ -56,12 +56,12 @@ async function createAndroidProject(repositoryName) {
 
   const folderStructure = {
     "app/src/main/AndroidManifest.xml": "\ntest\ntest\ntest\ntest\ntest",
-    [`app/src/main/java/${packageName.split(".").join("/")}`]: "",
     [`app/src/main/java/${packageName.split(".").join("/")}/MainActivity.java`]: "package " + packageName + ";\n\nimport androidx.appcompat.app.AppCompatActivity;\nimport android.os.Bundle;\n\npublic class MainActivity extends AppCompatActivity {\n    @Override\n    protected void onCreate(Bundle savedInstanceState) {\n        super.onCreate(savedInstanceState);\n        setContentView(R.layout.activity_main);\n    }\n}",
     "app/src/main/res/layout/activity_main.xml": '<?xml version="1.0" encoding="utf-8"?>\n<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"\n    xmlns:app="http://schemas.android.com/apk/res-auto"\n    xmlns:tools="http://schemas.android.com/tools"\n    android:layout_width="match_parent"\n    android:layout_height="match_parent"\n    tools:context=".MainActivity">\n\n    <TextView\n        android:layout_width="wrap_content"\n        android:layout_height="wrap_content"\n        android:layout_centerInParent="true"\n        android:text="Hello World!" />\n\n</RelativeLayout>\n',
     "gradle/build.gradle": "plugins {\n    id 'com.android.application'\n}\n\n"
   };
 
+  
   // Add the folder and file structure to the repository
   await Object.keys(folderStructure).forEach(async (path) => {
     const content = folderStructure[path];
